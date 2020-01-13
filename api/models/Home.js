e1cb6c38cc61a9e223ee03b1761cff6f6961b043
Mehul Agarwal <mehulagarwal@FC19L-MAC005.local>
2020-01-13 11:21:23 +0530

    Return package 'name' and 'description' in Version Check API

--- api/models/Home.js
+++ api/models/Home.js
@@ -7,6 +7,6 @@ export const HomeModel = {
 }
 
 async function getVersion () {
-  const { version } = packageJson
-  return { version }
+  const { name, description, version } = packageJson
+  return { name, description, version }
 }
