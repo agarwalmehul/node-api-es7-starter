1cba9a2895314d757207c2253a7ef459926ccc0c
Mehul Agarwal <mehulagarwal@FC19L-MAC005.lan>
2020-01-29 17:31:01 +0530

    Fix Status Code of HealthCheck API

--- api/controllers/Home.js
+++ api/controllers/Home.js
@@ -5,13 +5,13 @@ import { HomeModel } from '../models'
 
 export const HomeController = {
   healthCheck: asyncWrapper(healthCheck),
-  getVersion: asyncWrapper(getVersion),
+  getVersion: asyncWrapper(getVersion)
 }
 
 async function healthCheck (request, response, next) {
   if (response.body) { return process.nextTick(next) }
 
-  const responseBody = new ResponseBody(404, 'Health Check Successful')
+  const responseBody = new ResponseBody(200, 'Health Check Successful')
   response.body = responseBody
   process.nextTick(next)
 }
